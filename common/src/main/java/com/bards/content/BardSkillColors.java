7d7eadd82d03b3e52486e8afe724daebe7a21eb5
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2026-06-04 22:18:56 +0200

    Added missing Song Spell Colors

--- common/src/main/java/com/bards/content/BardSkillColors.java
+++ common/src/main/java/com/bards/content/BardSkillColors.java
@@ -3,7 +3,7 @@ package com.bards.content;
 import net.more_rpg_classes.custom.SpellBuilderHelper;
 import net.spell_engine.client.util.Color;
 
-public class BardSongColors {
+public class BardSkillColors {
     public static final Color troubadours_minuet = SpellBuilderHelper.BRIGHT_GREEN;
     public static final Color wanderers_minuet = SpellBuilderHelper.BRIGHT_GREEN;
     public static final Color natures_minne = SpellBuilderHelper.YELLOW_GREEN;
@@ -13,4 +13,10 @@ public class BardSongColors {
     public static final Color hymn_of_the_golden_light = SpellBuilderHelper.GOLD;
     public static final Color song_of_the_turning_sky = SpellBuilderHelper.MID_MAGENTA;
     public static final Color canticle_of_the_tides = SpellBuilderHelper.CYAN;
+    public static final Color magical_ballad = Color.ARCANE;
+    public static final Color vicious_mockery = Color.RAGE;
+    public static final Color wardens_paean = SpellBuilderHelper.CYAN; /// USE A DARK BLUE INSTEAD
+    public static final Color encore = SpellBuilderHelper.CYAN;
+    public static final Color armys_paeon = SpellBuilderHelper.GOLD;
+    public static final Color crescendo = SpellBuilderHelper.GOLD;
 }
